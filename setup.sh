9972c97cd1c241cd607001283347fc15adb8675c
KScott <kscott.db@gmail.com>
2019-12-21 08:01:24 +0000

    add waveshare driver, small modifications to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-echo Install BCM2835 libraries
+echo == Install BCM2835 libraries ==
 
 wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
 tar zxvf bcm2835-1.60.tar.gz
 cd bcm2835-1.60
-sudo ./configure
-sudo make
-sudo make check
+./configure
+make; make check
 sudo make install
 cd ..
-sudo rm -r bcm2835-1.60
+rm -r bcm2835-1.60
+rm bcm2835-1.60.tar.gz
 
-echo Install apt deps
+echo == Install apt deps ==
 
-sudo apt update
-sudo apt install wiringpi python3 python3-pip python3-numpy
+sudo apt update; sudo apt install -y \
+	wiringpi python3 python3-pip python3-numpy
 
-echo Install pip deps
+echo == Install pip deps ==
 
 sudo pip3 install RPi.GPIO spidev Pillow
+
+echo == Done ==
